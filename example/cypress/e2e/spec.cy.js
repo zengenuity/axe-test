ea2dc3cb4daf55b46e83c4f741a8980dc93f549d
Wayne Eaker <wayne@zengenuity.com>
2024-01-17 14:52:47 -0500

    Remove analyze command

--- example/cypress/e2e/spec.cy.js
+++ example/cypress/e2e/spec.cy.js
@@ -3,6 +3,6 @@ describe('Accessibility Test', () => {
     cy.visit('https://www.dev-wayne-wjg6iha-g2s6eyb7nc2mu.us.platformsh.site', {
       timeout: 30000,
     })
-    cy.axeAnalyze()
+    //cy.axeAnalyze()
   })
 })
