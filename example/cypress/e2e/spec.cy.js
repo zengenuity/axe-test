276d8a7f9a6330adda94f5619edc8e81ea7e22c3
Wayne Eaker <wayne@zengenuity.com>
2024-01-17 14:44:24 -0500

    Downgrade cypress

--- example/cypress/e2e/spec.cy.js
+++ example/cypress/e2e/spec.cy.js
@@ -1,9 +1,6 @@
 describe('Accessibility Test', () => {
   it('Visits home page', () => {
-    cy.visit('https://www.gurneysresorts.com')
+    cy.visit('https://www.dev-wayne-wjg6iha-g2s6eyb7nc2mu.us.platformsh.site')
     cy.axeAnalyze()
-    cy.visit('https://www.gurneysresorts.com/montauk')
-    cy.axeAnalyze()
-
   })
 })
