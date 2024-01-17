5e7627c4089182a234889498d1460bde8a1e00b4
Wayne Eaker <wayne@zengenuity.com>
2024-01-17 14:47:29 -0500

    Extend timeout

--- example/cypress/e2e/spec.cy.js
+++ example/cypress/e2e/spec.cy.js
@@ -1,6 +1,8 @@
 describe('Accessibility Test', () => {
   it('Visits home page', () => {
-    cy.visit('https://www.dev-wayne-wjg6iha-g2s6eyb7nc2mu.us.platformsh.site')
+    cy.visit('https://www.dev-wayne-wjg6iha-g2s6eyb7nc2mu.us.platformsh.site', {
+      timeout: 30000,
+    })
     cy.axeAnalyze()
   })
 })
